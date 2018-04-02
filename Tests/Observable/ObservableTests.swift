8a13d368c5d88821d21752524cb138567607b1ad
Maxim Bazarov <bazaroffma@gmail.com>
2018-04-02 12:42:41 +0200

    add Observable test

---
+++ Tests/Observable/ObservableTests.swift
@@ -0,0 +1,81 @@
+//
+//  FunctionalFoundationTests.swift
+//  FunctionalFoundationTests
+//
+//  Created by Maxim Bazarov on 4/2/18.
+//
+
+import XCTest
+@testable import FunctionalFoundation
+
+
+class ObservableTests: XCTestCase {
+    
+    func testAdd10ObserversAndChangeValue_ShouldBe10Values() {
+        let count = 10
+        let sut = Observable("sut")
+        
+        var result = [String]()
+        var unsubscribeTable = [Int: CancelSubscription]()
+        (1...count).forEach({ (index) in
+            let unsubscribe = sut.subscribe { value in
+                result.append(value)
+            }
+            unsubscribeTable[index] = unsubscribe
+        })
+        
+        sut.value = "test"
+        
+        XCTAssertEqual(result.count, count)
+    }
+    
+    func testAdd10ObserversAndChangeValue10Times_ShouldBe100Values() {
+        let sut = Observable("sut")
+        var result = [String]()
+        var unsubscribeTable = [Int: CancelSubscription]()
+        (1...10).forEach({ (index) in
+            let unsubscribe = sut.subscribe { value in
+                result.append(value)
+            }
+            unsubscribeTable[index] = unsubscribe
+        })
+        
+        (1...10).forEach({ (index) in
+            sut.value = "\(index)"
+        })
+        
+        
+        XCTAssertEqual(result.count, 100)
+    }
+    
+    func testChangeValue_ObservableValueShouldBeEqualNewValue() {
+        let sut = Observable(0)
+        sut.value = 10
+        XCTAssertEqual(sut.value, 10)
+    }
+    
+    func testIncrementValue_ValueShouldBeIncremented() {
+        let sut = Observable(0)
+        sut.value = sut.value + 10
+        XCTAssertEqual(sut.value, 10)
+    }
+    
+    
+    func testSubscribeToConditionValue_ShouldCallsAndOnlyForCondition() {
+        let sut = Observable(0)
+        let expectedCallsCount = 100
+        var callsCount = 0
+        
+        let _ = sut.subscribe(to: 7) { (v) in
+            XCTAssertEqual(v, 7)
+        }
+        
+        (1...expectedCallsCount).forEach({ (index) in
+            sut.value = index
+            callsCount += 1
+        })
+        
+        XCTAssertEqual(expectedCallsCount, callsCount)
+    }
+    
+}
---
+++ Tests/Observable/ObservableThreadSafetyTests.swift
@@ -0,0 +1,101 @@
+//
+//  ObservableThreadSafetyTests.swift
+//  FunctionalFoundation
+//
+//  Created by Maxim Bazarov on 4/2/18.
+//
+
+import XCTest
+@testable import FunctionalFoundation
+
+class ObservableThreadSafetyTests: XCTestCase {
+    
+    func testChangeValue101TimesOnRandomThread_SumObservedShouldBeEqualSumOfSended() {
+        let sut = Observable(0)
+        let lock = DispatchQueue(label: "ObservableTest.lockQueue")
+        var result = 0
+        var expected = 0
+        (0...100).forEach { (i) in
+            expected += i
+        }
+        
+        let _ = sut.subscribe { v in
+            lock.async {
+                result += v
+            }
+        }
+        
+        DispatchQueue.concurrentPerform(iterations: 101) { iteration in
+            sut.value = iteration
+        }
+        
+        lock.sync {
+            
+            print(expected)
+            XCTAssertEqual(result, expected)
+        }
+    }
+    
+    func testChangeValuesOnRandomThread_OrderObservedShouldBeSameAsSent() {
+        let sut = Observable(0)
+        let lock = DispatchQueue(label: "ObservableTest.lockQueue")
+        var result = [Int]()
+        var expected = [Int]()
+        
+        let _ = sut.subscribe { v in
+            lock.async {
+                result.append(v)
+            }
+        }
+        
+        DispatchQueue.concurrentPerform(iterations: 1000) { iteration in
+            lock.sync {
+                sut.value = iteration
+                expected.append(iteration)
+            }
+        }
+        
+        lock.sync {
+            XCTAssertEqual(result, expected)
+        }
+    }
+    
+    func disabledTestIncrementValueOnRandomThread_ValueShouldBeIncremented() {
+        let sut = Observable(0)
+        var expected = 0
+        (0...1000).forEach { (i) in
+            expected += i
+        }
+        
+        let lock = DispatchQueue(label: "ObservableTest.lockQueue")
+        lock.sync {
+            DispatchQueue.concurrentPerform(iterations: 1001) { iteration in
+                sut.value = sut.value + iteration
+                print("\(iteration): \(sut.value)")
+            }
+        }
+        
+        
+        XCTAssertEqual(sut.value, expected)
+    }
+    
+    func disabledTestRecursiveValueUpdate() {
+        let sut = Observable(0)
+        
+        let expected = 10
+        var actual = 0
+        
+        _ = sut.subscribe { v in
+            if v == 1 {
+                sut.value = 10
+            }
+        }
+        _ = sut.subscribe { v in
+            actual = v
+        }
+        
+        sut.value = 1
+        
+        XCTAssertEqual(actual, expected)
+    }
+}
